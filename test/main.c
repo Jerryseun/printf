231e18941c19a77029eab37daff6ffb1dc0cdaae
Jerryseun <adetunjijeremiah1@gmail.com>
2023-07-24 14:06:58 -0700

    custom conversion pointers

---
+++ test/main.c
@@ -0,0 +1,18 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char character;
+	const char* message;
+	char *ptr = "hello";
+
+	_printf("Unknown:[%r]\n");
+	return (0);
+}
