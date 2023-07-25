e391b8002ec405f65d823a15fb5f0de659192cd1
Jerryseun <adetunjijeremiah1@gmail.com>
2023-07-25 11:16:56 -0700

    update

--- print_num.c
+++ print_num.c
@@ -74,24 +74,15 @@ int print_int(va_list args)
 
 int print_decimal(va_list args)
 {
-	int count = 0;
-
 	int num = va_arg(args, int);
-
-	if (num < 0)
-	{
-		_putchar('-');
-		count++;
-		num = -num;
-	}
 
 	if (num == 0)
 	{
 		_putchar('0');
 		return (1);
 	}
+	else
+		print_number(num);
 
-	count = print_number(num);
-
-	return (count);
+	return (1);
 }
